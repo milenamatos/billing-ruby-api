66d1c521beea82f7438968337ad945985a034f4c
Milena <milematoss@hotmail.com>
2021-04-02 14:07:32 -0300

    feat: validacao dos documentos e data

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -2,6 +2,6 @@ class Institution < ApplicationRecord
   enum institution_type: { university: 'Universidade', school: 'Escola', dayCare: 'Creche' }
 
   validates :name, presence: true, uniqueness: true
-  validates :document, uniqueness: true, length: {is: 14}, allow_blank: true
+  validates :document, uniqueness: true, length: {is: 14}, allow_blank: true, numericality: { only_integer: true }
   validates :institution_type, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,9 @@ class Student < ApplicationRecord
   enum payment: {card: 'Cartão', ticket: 'Boleto'}
   
   validates :name, presence: true, uniqueness: true
-  validates :document, presence: true, uniqueness: true, length: {is: 11}
+  validates :document, presence: true, uniqueness: true, length: {is: 11}, numericality: { only_integer: true }
+  validates :birth_date, timeliness: { on_or_before: lambda { Date.current }, type: :date } , allow_blank: true
   validates :gender, presence: true
   validates :payment, presence: true
+
 end
