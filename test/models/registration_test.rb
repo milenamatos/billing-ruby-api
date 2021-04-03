647b7907b475463782b6470a5c40c74df30914a3
Milena <milematoss@hotmail.com>
2021-04-03 15:16:44 -0300

    feat: adiciona testes de validacao para model Registration

--- test/models/registration_test.rb
+++ test/models/registration_test.rb
@@ -1,7 +1,77 @@
 require "test_helper"
 
 class RegistrationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @registration = registrations(:one)
+  end
+
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save without any params" do
+    @registration = Registration.new
+    assert_not @registration.save and @registration.valid?
+  end
+
+  test "should not save without total_price" do
+    @registration.total_price = nil
+    assert_not @registration.save and @registration.valid?
+  end
+
+  test "should not save with invalid total_price" do
+    @registration.total_price = 0
+    assert_not @registration.save and @registration.valid?
+  end
+
+  test "should not save without bill_quantity" do
+    @registration.bill_quantity = nil
+    assert_not @registration.save and @registration.valid?
+  end
+
+  test "should not save with invalid bill_quantity" do
+    @registration.bill_quantity = 0
+    assert_not @registration.save and @registration.valid?
+  end
+
+  test "should not save without due_date" do
+    @registration.due_date = nil
+    assert_not @registration.save and @registration.valid?
+  end
+  
+  test "should not save with invalid due_date (not between 1 and 31) " do
+    @registration.due_date = 50
+    assert_not @registration.save and @registration.valid?
+  end
+
+  test "should not save without course_name" do
+    @registration.course_name = nil
+    assert_not @registration.save and @registration.valid?
+  end
+
+  test "should not save without institution" do
+    @registration.institution_id = nil
+    assert_not @registration.save and @registration.valid?
+  end
+
+  test "should not save with invalid institution (not found)" do
+    assert_raises (ActiveRecord::RecordNotFound) do
+      @registration.institution = Institution.find(name: 'not found')
+    end
+  end
+
+  test "should not save without student" do
+    @registration.student_id = nil
+    assert_not @registration.save and @registration.valid?
+  end
+
+  test "should not save with invalid student (not found)" do
+    assert_raises (ActiveRecord::RecordNotFound) do
+      @registration.student = Student.find(name: 'not found')
+    end
+  end
+
+  test "should save with correct values" do
+    assert @registration.save and @registration.valid?
+  end
 end
