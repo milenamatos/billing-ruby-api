268844c264586c2d637d463dc9d7f5e49b13045a
Milena <milematoss@hotmail.com>
2021-04-03 12:16:00 -0300

    fix: altera formato para sql

---
+++ db/structure.sql
@@ -0,0 +1,384 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: bill_type; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.bill_type AS ENUM (
+    'Aberta',
+    'Atrasada',
+    'Paga'
+);
+
+
+--
+-- Name: category; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.category AS ENUM (
+    'Universidade',
+    'Escola',
+    'Creche'
+);
+
+
+--
+-- Name: gender_type; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.gender_type AS ENUM (
+    'M',
+    'F'
+);
+
+
+--
+-- Name: payment_method; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.payment_method AS ENUM (
+    'Boleto',
+    'Cartão'
+);
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.ar_internal_metadata (
+    key character varying NOT NULL,
+    value character varying,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: bills; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.bills (
+    id bigint NOT NULL,
+    price double precision NOT NULL,
+    due_date date NOT NULL,
+    registration_id bigint NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL,
+    status public.bill_type DEFAULT 'Aberta'::public.bill_type NOT NULL
+);
+
+
+--
+-- Name: bills_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.bills_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: bills_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.bills_id_seq OWNED BY public.bills.id;
+
+
+--
+-- Name: institutions; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.institutions (
+    id bigint NOT NULL,
+    name character varying NOT NULL,
+    document character varying(14),
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL,
+    institution_type public.category NOT NULL
+);
+
+
+--
+-- Name: institutions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.institutions_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: institutions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.institutions_id_seq OWNED BY public.institutions.id;
+
+
+--
+-- Name: registrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.registrations (
+    id bigint NOT NULL,
+    total_price double precision NOT NULL,
+    bill_quantity integer NOT NULL,
+    due_date integer NOT NULL,
+    course_name character varying NOT NULL,
+    institution_id bigint NOT NULL,
+    student_id bigint NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: registrations_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.registrations_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: registrations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.registrations_id_seq OWNED BY public.registrations.id;
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.schema_migrations (
+    version character varying NOT NULL
+);
+
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.students (
+    id bigint NOT NULL,
+    name character varying NOT NULL,
+    document character varying(11) NOT NULL,
+    birth_date date,
+    telephone integer,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL,
+    gender public.gender_type NOT NULL,
+    payment public.payment_method NOT NULL
+);
+
+
+--
+-- Name: students_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.students_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: students_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
+
+
+--
+-- Name: bills id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.bills ALTER COLUMN id SET DEFAULT nextval('public.bills_id_seq'::regclass);
+
+
+--
+-- Name: institutions id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.institutions ALTER COLUMN id SET DEFAULT nextval('public.institutions_id_seq'::regclass);
+
+
+--
+-- Name: registrations id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.registrations ALTER COLUMN id SET DEFAULT nextval('public.registrations_id_seq'::regclass);
+
+
+--
+-- Name: students id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.students_id_seq'::regclass);
+
+
+--
+-- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.ar_internal_metadata
+    ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
+
+
+--
+-- Name: bills bills_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.bills
+    ADD CONSTRAINT bills_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: institutions institutions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.institutions
+    ADD CONSTRAINT institutions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: registrations registrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.registrations
+    ADD CONSTRAINT registrations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.students
+    ADD CONSTRAINT students_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: index_bills_on_registration_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_bills_on_registration_id ON public.bills USING btree (registration_id);
+
+
+--
+-- Name: index_institutions_on_document; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_institutions_on_document ON public.institutions USING btree (document);
+
+
+--
+-- Name: index_institutions_on_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_institutions_on_name ON public.institutions USING btree (name);
+
+
+--
+-- Name: index_registrations_on_institution_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_registrations_on_institution_id ON public.registrations USING btree (institution_id);
+
+
+--
+-- Name: index_registrations_on_student_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_registrations_on_student_id ON public.registrations USING btree (student_id);
+
+
+--
+-- Name: index_students_on_document; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_students_on_document ON public.students USING btree (document);
+
+
+--
+-- Name: index_students_on_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_students_on_name ON public.students USING btree (name);
+
+
+--
+-- Name: registrations fk_rails_5529bb134f; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.registrations
+    ADD CONSTRAINT fk_rails_5529bb134f FOREIGN KEY (student_id) REFERENCES public.students(id);
+
+
+--
+-- Name: registrations fk_rails_9a77cd7a8d; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.registrations
+    ADD CONSTRAINT fk_rails_9a77cd7a8d FOREIGN KEY (institution_id) REFERENCES public.institutions(id);
+
+
+--
+-- Name: bills fk_rails_d8a8a4f7ce; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.bills
+    ADD CONSTRAINT fk_rails_d8a8a4f7ce FOREIGN KEY (registration_id) REFERENCES public.registrations(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+SET search_path TO "$user", public;
+
+INSERT INTO "schema_migrations" (version) VALUES
+('20210401232650'),
+('20210401232932'),
+('20210402000440'),
+('20210402001009'),
+('20210402002922'),
+('20210402013208'),
+('20210402021930'),
+('20210402022014'),
+('20210402023846');
+
+
