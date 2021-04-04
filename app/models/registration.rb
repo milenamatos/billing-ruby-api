77c96ed33cb5fefe95499180c48ba2baf8b12418
Milena <milematoss@hotmail.com>
2021-04-04 11:21:41 -0300

    refactor: adiciona logica de criacao de bills na model registration

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -16,4 +16,23 @@ class Registration < ApplicationRecord
   validates :student, presence: true
 
   has_many :bill
+
+  after_create :create_bills
+
+  def create_bills 
+    current_date = DateTime.now
+		current_day = current_date.strftime("%d").to_i
+	  current_month = current_date.strftime("%m").to_i
+
+		first_bill_date = current_date.change(day: self.due_date)
+		bill_date = (self.due_date > current_day) ? first_bill_date : first_bill_date.next_month
+					
+		price = self.total_price / self.bill_quantity
+
+    for i in 1..self.bill_quantity do
+      self.bill.build({registration_id: self.id, due_date: bill_date.strftime("%Y/%m/%d"), price: price})
+      bill_date = bill_date.next_month
+    end 
+    self.save
+  end 
 end
